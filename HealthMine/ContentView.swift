232acd9b8a119902910151403942d30125d63020
Lee seung hwan <hwan00716@gmail.com>
2026-08-05 18:57:00 +0900

    Initial Commit

---
+++ HealthMine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HealthMine
+//
+//  Created by 이승환 on 8/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HealthMine/HealthMineApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthMineApp.swift
+//  HealthMine
+//
+//  Created by 이승환 on 8/5/26.
+//
+
+import SwiftUI
+
+@main
+struct HealthMineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
